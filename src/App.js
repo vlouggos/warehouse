36f69a1d0b6eb6d749f78c33c7e31bfe57c8f9de
vlouggos <44415066+vlouggos@users.noreply.github.com>
2023-03-05 01:06:10 +0200

    4th Commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
-import React from 'react';
-import { Amplify } from 'aws-amplify';
+import React from "react";
+import { Amplify } from "aws-amplify";
 
-import { Authenticator } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
+import { Authenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+import { YinYang } from "react-bootstrap-icons";
 
-import awsExports from './aws-exports';
+import NavigationBar from "./components/NavigationBar";
+import './App.css';
+import awsExports from "./aws-exports";
 Amplify.configure(awsExports);
 
 export default function App() {
   return (
     <Authenticator>
-      {({ signOut, user }) => (
-        <main>
-          <h1>Hello {user.username}</h1>
-          <button onClick={signOut}>Sign out</button>
-        </main>
-      )}
+      
+          {({ signOut, user }) => (
+            <main>
+              <NavigationBar name={user.attributes.email}/>
+              <YinYang />
+              <h1>Hello {user.username}</h1>
+              <button onClick={signOut}>Sign out</button>
+            </main>
+          )}
+        
     </Authenticator>
   );
 }
